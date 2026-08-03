6a674a973f7f87677a1de727747da941d11b8bb9
KARANAM SAI MANIKANTA <saikaranam46@gmail.com>
2026-08-03 14:15:37 +0530

    [19] [Time Beats: 100.00%] [Memory Beats: 40.57%] - LeetPush

--- Linked_List/BasicOperation/19-remove-nth-node-from-end-of-list/19-Remove-Nth-Node-From-End-of-List.java
+++ Linked_List/BasicOperation/19-remove-nth-node-from-end-of-list/19-Remove-Nth-Node-From-End-of-List.java
@@ -10,34 +10,21 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head==null || n<=0) return head;
-        ListNode reversed = reverse(head);
-        ListNode temp = reversed;
-        if(n==1){
-            reversed = reversed.next;
-        }else{
-            int cnt = 1;
-            while(temp !=null && cnt < n-1){
-                temp = temp.next;
-                cnt++;
-            }
-            if(temp!=null && temp.next!=null){
-                temp.next = temp.next.next;
-            }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        for(int i = 0 ;i<=n;i++){
+            fast = fast.next;
         }
-        
-        
-        return reverse(reversed);
-    }
-    public static ListNode reverse(ListNode list){
-        ListNode curr = list;
-        ListNode prev = null;
-        while(curr !=null){
-            ListNode temp = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = temp;
+
+        while(fast != null){
+            fast = fast.next;
+            slow = slow.next;
         }
-        return prev;
+        slow.next = slow.next.next;
+        return dummy.next;
     }
 }
