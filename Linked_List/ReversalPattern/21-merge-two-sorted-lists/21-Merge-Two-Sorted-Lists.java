e4b450c5dceea53a30384f069a5dde2445f9f6cd
KARANAM SAI MANIKANTA <saikaranam46@gmail.com>
2026-08-07 12:36:23 +0530

    [21] [Time Beats: 100.00%] [Memory Beats: 76.10%] - LeetPush

---
+++ Linked_List/ReversalPattern/21-merge-two-sorted-lists/21-Merge-Two-Sorted-Lists.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+
+        ListNode first = list1;
+        ListNode second = list2;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        
+        while(first!=null && second!=null){
+            if(first.val<=second.val){
+                curr.next = first;
+                curr = curr.next;
+                first = first.next;
+            }else{
+                curr.next = second;
+                curr = curr.next;
+                second = second.next;
+            }
+        }
+
+        while(first!=null){
+            curr.next = first;
+            curr = curr.next;
+            first = first.next;
+        }
+        while(second!=null){
+            curr.next = second;
+            curr = curr.next;
+            second = second.next;
+        }
+        return dummy.next;
+
+
+    }
+}
