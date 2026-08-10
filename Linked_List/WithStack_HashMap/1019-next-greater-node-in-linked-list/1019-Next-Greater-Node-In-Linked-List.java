bf734de6f30576edf460be5dfb8e23f610385610
KARANAM SAI MANIKANTA <saikaranam46@gmail.com>
2026-08-10 14:49:20 +0530

    [1019] [Time Beats: 54.32%] [Memory Beats: 6.72%] - LeetPush

---
+++ Linked_List/WithStack_HashMap/1019-next-greater-node-in-linked-list/1019-Next-Greater-Node-In-Linked-List.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while(head!=null){
+            list.add(head.val);
+            head = head.next;
+        }
+
+        int n = list.size();
+        int ans[] = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i = n-1; i>=0;i--){
+            while(!stack.isEmpty() && stack.peek() <= list.get(i)) stack.pop();
+
+            if(!stack.isEmpty()){
+                ans[i] = stack.peek();
+            }
+            stack.push(list.get(i));
+        }
+        return ans;
+    }
+}
