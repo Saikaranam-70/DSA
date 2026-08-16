f3b0f29d5b29aa65bb3a36725399bfaebca03a5a
KARANAM SAI MANIKANTA <saikaranam46@gmail.com>
2026-08-16 22:25:54 +0530

    [62] [Time Beats: 100.00%] [Memory Beats: 13.06%] - LeetPush

---
+++ Recursion/Non_LinearRecursion/62-unique-paths/62-Unique-Paths.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        return countPaths(m-1, n-1, dp);
+    }
+    int countPaths(int row, int col, int[][] dp){
+        if(row == 0 || col == 0){
+            return 1;
+        }
+
+        if(dp[row][col] != 0){
+            return dp[row][col];
+        }
+
+        dp[row][col] = countPaths(row-1, col, dp) + countPaths(row, col-1, dp);
+
+        return dp[row][col];
+    }
+}
+
+
+//Non Linear Recursion
+/*class Solution {
+    public int uniquePaths(int m, int n) {
+        return countPaths(m-1, n-1);
+    }
+    int countPaths(int row, int col){
+        if(row == 0 || col == 0) return 1;
+
+        return countPaths(row-1, col) + countPaths(row, col-1);
+    }
+}*/
