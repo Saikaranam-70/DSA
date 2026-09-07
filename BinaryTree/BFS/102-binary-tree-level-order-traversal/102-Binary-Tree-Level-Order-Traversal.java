3ad652adb292464f81b037e139d7cda5ef886188
KARANAM SAI MANIKANTA <saikaranam46@gmail.com>
2026-09-07 18:35:25 +0530

    [102] [Time Beats: 95.73%] [Memory Beats: 80.08%] - LeetPush

---
+++ BinaryTree/BFS/102-binary-tree-level-order-traversal/102-Binary-Tree-Level-Order-Traversal.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if(root == null) return list;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> curr = new ArrayList<>();
+            for(int i = 0;i<size;i++){
+                TreeNode currNode = queue.poll();
+                curr.add(currNode.val);
+                if(currNode.left!=null) queue.offer(currNode.left);
+                if(currNode.right!=null) queue.offer(currNode.right);
+            }
+            list.add(curr);
+        }
+        return list;
+    }
+}
