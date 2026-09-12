977f647ef08e99624da2e2f41a9d1ce9929b0d8d
KARANAM SAI MANIKANTA <saikaranam46@gmail.com>
2026-09-12 22:20:35 +0530

    [987] [Time Beats: 57.64%] [Memory Beats: 48.84%] - LeetPush

--- BinaryTree/BFS/987-vertical-order-traversal-of-a-binary-tree/987-Vertical-Order-Traversal-of-a-Binary-Tree.java
+++ BinaryTree/BFS/987-vertical-order-traversal-of-a-binary-tree/987-Vertical-Order-Traversal-of-a-Binary-Tree.java
@@ -14,6 +14,54 @@
  * }
  */
 class Solution {
+    class Pair{
+        TreeNode root;
+        int row;
+        int col;
+        Pair(TreeNode root, int row, int col){
+            this.root = root;
+            this.row = row;
+            this.col = col;
+        }
+    }
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+
+      List<int[]> list = new ArrayList<>();
+      Queue<Pair> queue = new LinkedList<>();
+      queue.offer(new Pair(root, 0, 0));
+
+      while(!queue.isEmpty()){
+        Pair curr = queue.poll();
+        list.add(new int[]{curr.col, curr.row, curr.root.val});
+        if(curr.root.left!=null)
+        queue.offer(new Pair(curr.root.left, curr.row+1, curr.col-1));
+        if(curr.root.right!=null)
+        queue.offer(new Pair(curr.root.right, curr.row+1, curr.col+1));
+      }
+
+      Collections.sort(list, (a, b)->{
+        if (a[0] != b[0]){
+            return a[0] - b[0];
+        }
+        if(a[1] != b[1]) return a[1]-b[1];
+        return a[2]-b[2];
+      });
+      List<List<Integer>> ans = new ArrayList<>();
+      int prevCol = Integer.MIN_VALUE;
+      for(int[] node: list){
+        if(node[0] != prevCol){
+            ans.add(new ArrayList<>());
+            prevCol = node[0];
+        }
+        ans.get(ans.size()-1).add(node[2]);
+      }
+      return ans;
+    }
+
+}
+
+/* DFS
+class Solution {
     List<int[]> list = new ArrayList<>();
     public List<List<Integer>> verticalTraversal(TreeNode root) {
        dfs(root, 0, 0);
@@ -44,4 +92,4 @@ class Solution {
         dfs(root.left, row+1, col-1);
         dfs(root.right, row+1, col+1);
     }
-}
+} */
